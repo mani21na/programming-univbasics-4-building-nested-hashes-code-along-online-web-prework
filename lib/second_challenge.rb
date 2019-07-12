1eab971e5811fd81a7f2e607ea6f8a6ec8f91609
Hanna Lee <mani21na@gmail.com>
2019-07-12 21:34:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -15,7 +15,7 @@ def second_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :hero_friends => [
                         {name: "Steven", age: "30", attitude: "confused"}                        
-                        {name: "Mercutio", age: "18", attitude: "hot-headed"} 
+                        {name: "Nurse", age: "18", attitude: "hot-headed"} 
                        ]
     }
   }
