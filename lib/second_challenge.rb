f0aa15a1f56ae530a8706e6c8418bcd3f1675070
Hanna Lee <mani21na@gmail.com>
2019-07-12 21:34:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -13,7 +13,9 @@ def second_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :hero_friends => []
+      :hero_friends => [
+                        {name: "Mercutio", age: "18", attitude: "hot-headed"} 
+                       ]
     }
   }
 end
