c4704e6cd259f91950e2c514af5a959a930ce990
Hanna Lee <mani21na@gmail.com>
2019-07-12 21:46:21 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  puts epic_tragedy
+  puts epic_tragedy[montague]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
