9a24ea1613d0d28c74973bdfe362729bcfceb36d
Hanna Lee <mani21na@gmail.com>
2019-07-12 21:29:35 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {name: "Lord Montague", age: 53},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15"},
       :hero_friends => []
     }, 
     :capulet => {
