cb1ffbf9de0e912599c4281824bae07f3f3087f1
Hanna Lee <mani21na@gmail.com>
2019-07-12 21:48:48 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  puts epic_tragedy[:montague][:hero][2]
+  epic_tragedy[:montague][:hero][:status] = dead
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
